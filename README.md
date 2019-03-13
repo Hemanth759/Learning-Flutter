4ee5da6b4cd42d9c39ddfe389ede983f5ad0b542
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-03-14 03:02:03 +0530

    Deleted all

--- README.md
+++
@@ -1,4 +0,0 @@
-# Flutter-Projects
-A repo for all my flutter projects
-
-Travis CL: [![Build Status](https://travis-ci.org/Hemanth759/Flutter-Projects.svg?branch=master)](https://travis-ci.org/Hemanth759/Flutter-Projects)
--- test/app_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:todo_app/main.dart';
-
-void main() {
-  testWidgets('app should work', (tester) async {
-    await tester.pumpWidget(new MyApp());
-    expect(find.text('Hello, World!'), findsOneWidget);
-  });
-}
