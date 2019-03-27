abaaa863092f24ad5ae40c9314fdf4ca9751d58a
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-03-27 19:36:17 +0530

    Added model for the note

---
+++ todo_app/lib/models/note.dart
@@ -0,0 +1,48 @@
+class Note {
+  int _id;
+  int _priority;
+  String _title;
+  String _description;
+  String _date;
+
+  // Creating the constructor
+  Note(this._title, this._date, this._priority, [this._description]);
+  Note.withId(this._id, this._title, this._date, this._priority,
+      [this._description]);
+
+  // Setting get fucntions for the variables
+  int get id => _id;
+  int get priority => _priority;
+  String get title => _title;
+  String get description => _description;
+  String get date => _date;
+
+  //Setting set functions for the variables
+  set title(String newTitle) {
+    if (newTitle.length <= 255) {
+      this._title = newTitle;
+    }
+  }
+
+  set priority(int newPriority) {
+    if (newPriority >= 1 && newPriority <= 2) {
+      this._priority = newPriority;
+    }
+  }
+
+  set date(String newdate) {
+    this._date = newdate;
+  }
+
+  //Creating function to convert note object to map object
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = new Map<String, dynamic>();
+    if (_id != null) {
+      map['id'] = _id;
+    }
+    map['title'] = _title;
+    map['priority'] = _priority;
+    map['description'] = _description;
+    map['date'] = _date;
+  }
+}
