8b2bd1565ef321db2a30b8afb6e8151e0d543211
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-06-09 21:50:47 +0530

    Removed firebase auth import

--- real_time_messaging/lib/services/secureStorageCRUD.dart
+++ real_time_messaging/lib/services/secureStorageCRUD.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:real_time_messaging/services/firestoreCRUD.dart';
 
